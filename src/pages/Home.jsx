56edce4c6351f4825080acc8a46e0f866ad0d473
smartcraze <smartcraze>
2024-03-09 04:53:57 +0530

    done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom"; // Import Link
 import Card from "../components/Cards";
+import Banner from "./Banner";
 
 function Home() {
   return (
     <>
-    <section className="bg-white dark:bg-gray-900">
+    <Banner/>
+    <section className="bg-white dark:bg-gray-900 ">
     <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"> Welcome To Ai     TechHub</h1>
