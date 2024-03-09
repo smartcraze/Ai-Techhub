48f68d5d84ad231f6ef19a11bbcc9239c9ea036f
smartcraze <smartcraze>
2024-03-09 06:00:03 +0530

    added profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { useAuth } from '../utils/AuthContext'; 
+import { useAuth } from '../utils/AuthContext'; // Update the path to your AuthContext file
 
 const Profile = () => {
     const { user } = useAuth();
 
     return (
-        <div>
-            {user ? (
-                <p>Welcome, {user.name}</p> // Assuming 'name' is the property that holds the username
-            ) : (
-                <p>Please log in</p>
-            )}
+        <div className="flex items-center justify-center h-[90vh] bg-gray-100">
+            <div className="max-w-xs w-full">
+                <div className="bg-white shadow-xl rounded-lg overflow-hidden">
+                    <div className="p-4 text-center">
+                        <img className="w-32 h-32 mx-auto rounded-full" src="user.png" alt={user ? user.name : 'User Profile'} />
+                        <h3 className="text-xl text-gray-900 font-medium mt-4">{user ? user.name : 'John Doe'}</h3>
+                        <p className="text-gray-600">{user ? user.email : 'john@example.com'}</p>
+                        <p className="text-gray-600">{user ? user.phone : '+977 9955221114'}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
