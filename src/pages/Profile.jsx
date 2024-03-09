cf80564065d6efa5c71bec85a1ca072ac0442923
smartcraze <smartcraze>
2024-03-09 06:12:23 +0530

    done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,11 +5,11 @@ const Profile = () => {
     const { user } = useAuth();
 
     return (
-        <div className="flex items-center justify-center h-[90vh] bg-gray-100">
+        <div className="flex items-center justify-center h-[90vh] bg-purple-400">
             <div className="max-w-xs w-full">
-                <div className="bg-white shadow-xl rounded-lg overflow-hidden">
+                <div className="profile-card bg-white shadow-xl rounded-lg overflow-hidden transform transition-transform duration-500 hover:rotate-y-180 perspective-1000 hover:shadow-2xl">
                     <div className="p-4 text-center">
-                        <img className="w-32 h-32 mx-auto rounded-full" src="user.png" alt={user ? user.name : 'User Profile'} />
+                        <img className="w-32 h-32 mx-auto rounded-full transition-transform duration-500 hover:rotate-360" src="user.png" alt={user ? user.name : 'User Profile'} />
                         <h3 className="text-xl text-gray-900 font-medium mt-4">{user ? user.name : 'John Doe'}</h3>
                         <p className="text-gray-600">{user ? user.email : 'john@example.com'}</p>
                         <p className="text-gray-600">{user ? user.phone : '+977 9955221114'}</p>
